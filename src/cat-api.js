8b72e7c5ca4f926ff0581e1f7d802ae01dbea76e
KatalinLengyel <158131186+KatalinLengyel@users.noreply.github.com>
2024-07-13 14:07:00 +0300

    Tema10

---
+++ src/cat-api.js
@@ -0,0 +1,27 @@
+import Notiflix from 'notiflix';
+
+const API_KEY =
+  'live_neQtqXeixoBIqFwhroHRrrmvwrqyAqKNlOtw1h0pUptDaBOy2lz9kT2qr4tKfpji';
+
+async function getBreeds() {
+  const breeds = await fetch(
+    `https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`
+  );
+  return await breeds.json();
+}
+
+async function getCat(id) {
+  try {
+    const breeds = await fetch(
+      `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${id}`
+    );
+    return await breeds.json();
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  }
+}
+
+export {getBreeds, getCat};
